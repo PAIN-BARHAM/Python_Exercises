004292640e817fcd266c0e95f13a9210b0d95d43
Pain <mohammedbrham98@gmail.com>
2022-11-14 11:20:42 +0300

    add second example for the function

--- decorators.py
+++ decorators.py
@@ -1,17 +1,25 @@
+import functools
+
+
 def start_end_decorator(func):
 
-    def wrapper():
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
         print('Start')
-        func()
+        result = func(*args, **kwargs)
         print('End')
+        return result
     return wrapper
 
 
 @start_end_decorator
-def print_name():
-    print('Alex')
+def add5(x):
+    return x + 5
 
 
 # print_name = start_end_decorator(print_name)
 
-print_name()
+print(add5(2))
+
+print(add5.__name__)
+print(help(add5))
