7bc1bb3683261de46870b4bd5fc90c755dcd460c
Pain <mohammedbrham98@gmail.com>
2022-11-14 12:18:30 +0300

    nested decorators

--- decorators.py
+++ decorators.py
@@ -13,14 +13,25 @@ def start_end_decorator(func):
     return wrapper
 
 
+def debug(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        args_repr = [repr(a) for a in args]
+        kwargs_repr = [f"{k}={v!r}" for k, v in kwargs.items()]
+        signature = ", ".join(args_repr + kwargs_repr)
+        print(f"Calling {func.__name__}({signature})")
+        result = func(*args, **kwargs)
+        print(f"{func.__name__!r} returned {result!r}")
+        return result
+    return wrapper
+
+
+@debug
 @start_end_decorator
-def add5(x):
-    return x + 5
+def greet(name):
+    greeting = f'Hello {name}'
+    print(greeting)
+    return greeting
 
 
-# print_name = start_end_decorator(print_name)
-
-print(add5(2))
-
-print(add5.__name__)
-print(help(add5))
+print(greet('Alex'))
